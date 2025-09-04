dae43910028afe6441c7f9e9be81249f96587c90
Shahid7294 <sahidzoo123@gmail.com>
2025-09-04 12:43:03 +0530

    todo_app

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path("",views.home,name='home'),
     path("addtask/",views.addtask,name='addtask'),
-    path("<int:pk>/",views.edittask,name='edittask'),
+    path("Mark_as_Done/<int:pk>/",views.Mark_as_Done,name='Mark_as_Done'),
+    path("Mark_as_UnDone/<int:pk>/",views.Mark_as_UnDone,name='Mark_as_UnDone'),
+    path("edittask/<int:pk>/",views.edittask,name='edittask'),
+    path("del/<int:pk>/",views.del_task,name='del_task')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404,HttpResponse
 from .models import R_Task
 # Create your views here.
 
 def home(request):
     tasks=R_Task.objects.filter(is_completed=False).order_by('updated_at')
+    is_completed=R_Task.objects.filter(is_completed=True)
     context={
-        'tasks':tasks
+        'tasks':tasks,
+        'is_completed':is_completed
     }
     return render(request,'home.html',context)
 
@@ -13,6 +15,33 @@ def addtask(request):
     tasks=request.POST['task']
     R_Task.objects.create(task=tasks)
     return redirect("home")
+def Mark_as_Done(request,pk):
+    task=get_object_or_404(R_Task,pk=pk)
+    task.is_completed=True
+    task.save()
+    return redirect('home')
+
+def Mark_as_UnDone(request,pk):
+    task=R_Task.objects.get(pk=pk)
+    # task=get_object_or_404(R_Task,pk=pk)
+    task.is_completed=False
+    task.save()
+    return redirect('home')
 
 def edittask(request,pk):
-    return render(request,'edittask.html')
+    get_task=R_Task.objects.get(pk=pk)
+    if request.method == "POST":
+        new_task=request.POST['task']
+        get_task.task=new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context={
+            'get_task':get_task
+        }
+        return render(request,'edittask.html',context)
+    
+def del_task(request,pk):
+    task=R_Task.objects.get(pk=pk)
+    task.delete()
+    return redirect('home')
